83b65cddec09e0f2ae8a56433460de8654610475
Sam Wechsler <solarsamuel@gmail.com>
2024-03-02 18:07:09 -0500

    Update load_and_plot_W1_from_CSV.py
    Added W1 to be more specific in regard to which weights

--- load_and_plot_W1_from_CSV.py
+++ load_and_plot_W1_from_CSV.py
@@ -4,30 +4,30 @@ from matplotlib import pyplot as plt
 
 def init_params_from_csv():
     # Load weights from CSV files
-    W_data = []
+    W1_data = []
     with open('W1_output84.csv', 'r') as file:
         for line in file:
             if line.strip().startswith('Weights'):
                 continue  # Skip the line if it starts with 'neuron'
-            W_data.append(list(map(float, line.strip().split(','))))
-    W = np.array(W_data)
+            W1_data.append(list(map(float, line.strip().split(','))))
+    W1 = np.array(W1_data)
     
     # Divide into 10 separate weights
-    weights = []
+    weights1 = []
     for i in range(10):
-        weight = W[i*28:(i+1)*28, :]
-        weights.append(weight)
+        weight = W1[i*28:(i+1)*28, :]
+        weights1.append(weight)
     
-    return weights
+    return weights1
 
 # Call the function to load parameters from CSV files
-weights = init_params_from_csv()
+weights1 = init_params_from_csv()
 
 # Visualize the weights
 plt.figure(figsize=(15, 7))
-for i, weight in enumerate(weights):
+for i, weight1 in enumerate(weights1):
     plt.subplot(2, 5, i + 1)
-    plt.imshow(weight, cmap='gray')
+    plt.imshow(weight1, cmap='gray')
     plt.title(f'Weight {i}')
     plt.axis('off')
 plt.suptitle('Visualization of Weights')
